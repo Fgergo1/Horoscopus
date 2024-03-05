db816ce9bca7467037d3e21852ea05deb1ba155c
Fgergo1 <128323984+Fgergo1@users.noreply.github.com>
2024-03-05 14:57:22 +0100

    Add files via upload
    basic setup of the project is done

---
+++ backend/src/main/java/org/example/horoscopus/HoroscopusApplication.java
@@ -0,0 +1,13 @@
+package org.example.horoscopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoroscopusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoroscopusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/example/horoscopus/HoroscopusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.horoscopus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoroscopusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
