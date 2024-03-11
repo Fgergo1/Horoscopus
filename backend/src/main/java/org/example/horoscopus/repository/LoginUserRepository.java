d0631eb2420c462e23ad1c9ff14b95bb562b30cf
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-03-11 19:55:30 +0100

    unnecessary classes are removed

--- backend/src/main/java/org/example/horoscopus/repository/LoginUserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.horoscopus.repository;
-
-import org.example.horoscopus.model.LoginUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginUserRepository extends JpaRepository<LoginUser, Long> {
-}
