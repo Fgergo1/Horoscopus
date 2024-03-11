3c8a51703b099c89edd5886902c9e5981b306225
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-03-11 19:58:23 +0100

    LoginUserDTO and RegisterUserDTO created

---
+++ backend/src/main/java/org/example/horoscopus/DTO/LoginUserDTO.java
@@ -0,0 +1,14 @@
+package org.example.horoscopus.DTO;
+
+public class LoginUserDTO {
+    private String password;
+    private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
