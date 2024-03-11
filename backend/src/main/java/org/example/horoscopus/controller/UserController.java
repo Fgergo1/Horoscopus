cb910f073993258ca59c356d8378737a807247aa
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-03-11 19:57:02 +0100

    user login and register endpoints  created

--- backend/src/main/java/org/example/horoscopus/controller/UserController.java
+++ backend/src/main/java/org/example/horoscopus/controller/UserController.java
@@ -1,5 +1,13 @@
 package org.example.horoscopus.controller;
 
+import org.example.horoscopus.DTO.LoginUserDTO;
+import org.example.horoscopus.DTO.RegisterUserDTO;
+import org.example.horoscopus.model.User;
+import org.example.horoscopus.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
 
+   private final UserService userService;
 
-    public void saveUser () {
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> saveUser (@RequestBody RegisterUserDTO registerUserDTO) {
+
+        if (userService.saveNewUser(registerUserDTO)) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
 
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser (@RequestBody LoginUserDTO loginUserDTO) {
+
+        // need to set up the security
+    }
+
 }
