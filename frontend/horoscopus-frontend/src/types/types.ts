c11937255497c88e7932b3022212d8f3032d504c
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-03-11 11:21:15 +0100

    types.ts added for storage the typeScript types

---
+++ frontend/horoscopus-frontend/src/types/types.ts
@@ -0,0 +1,12 @@
+
+export type RegistrationUser = {
+    username : String,
+    password : String,
+    rePassword : String,
+    email : String
+}
+
+export type LoginUser = {
+    password : String,
+    email : String
+}
