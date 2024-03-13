9f8829634d1c8d80d90904e2494237cbaed35d4f
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-03-13 14:25:09 +0100

    constructor added to LoginUserDTO for testing

--- backend/src/main/java/org/example/horoscopus/DTO/LoginUserDTO.java
+++ backend/src/main/java/org/example/horoscopus/DTO/LoginUserDTO.java
@@ -5,6 +5,10 @@ public class LoginUserDTO {
     private String userName;
     private String password;
 
+    public LoginUserDTO(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
 
     public String getPassword() {
         return password;
