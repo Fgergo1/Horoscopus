60201df2b5dcfd51344a07092ced522668512fc3
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-03-13 14:25:57 +0100

    constructor added to RegisterUserDTO for testing

--- backend/src/main/java/org/example/horoscopus/DTO/RegisterUserDTO.java
+++ backend/src/main/java/org/example/horoscopus/DTO/RegisterUserDTO.java
@@ -5,6 +5,12 @@ public class RegisterUserDTO {
     private String password;
     private String email;
 
+    public RegisterUserDTO(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
     public String getUsername() {
         return username;
     }
