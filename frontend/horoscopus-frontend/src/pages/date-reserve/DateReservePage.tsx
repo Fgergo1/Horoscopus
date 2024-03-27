617e218b8085fbef9b52e4b46633d457ef807a92
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-03-27 12:36:54 +0100

    Date reserve page created

---
+++ frontend/horoscopus-frontend/src/pages/date-reserve/DateReservePage.tsx
@@ -0,0 +1,29 @@
+import {ReserveDate} from "../../types/types.ts";
+interface ReservePageProps {
+    dates : ReserveDate[],
+    error : string | null
+}
+
+const  DateReservePage  = (props : ReservePageProps) => {
+
+    const {dates, error} = props;
+
+    return error ? (
+        <>
+            <div className="date-container">
+                <div className="wrap-date">
+                    <p>{"Free dates :"}</p>
+                    {dates.map((date: ReserveDate) => (
+                        <>
+                            <p>{date.start}</p>
+                            <p>{date.end}</p>
+                        </>
+                    ))}
+                </div>
+            </div>
+        </>
+    ) : (
+        <p>{error}</p>
+    )
+}
+export default DateReservePage
