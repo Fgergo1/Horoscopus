719e32bc0e874f949bc02f5e23355c7b49c89d65
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-03-27 14:07:42 +0100

    date service is created

---
+++ backend/src/main/java/org/example/horoscopus/service/DateService.java
@@ -0,0 +1,33 @@
+package org.example.horoscopus.service;
+import org.example.horoscopus.DTO.FreeDateDTO;
+import org.example.horoscopus.model.FreeDateEntity;
+import org.example.horoscopus.repository.DateRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class DateService {
+
+    private final DateRepository repository;
+
+    public DateService(DateRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<FreeDateDTO> getFreeDatesFromDatabase () {
+       return repository.getAll().stream().map((freeDateEntity -> new FreeDateDTO(freeDateEntity.getTimeInterval())))
+               .toList();
+  }
+
+  public boolean deleteDate (FreeDateDTO freeDateDTO) {
+        return repository.deleteByTimeInterval(freeDateDTO.getDate());
+  }
+  public boolean saveNewDate (FreeDateDTO freeDateDTO) {
+        try {
+            repository.save(new FreeDateEntity(freeDateDTO.getDate()));
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+  }
+}
