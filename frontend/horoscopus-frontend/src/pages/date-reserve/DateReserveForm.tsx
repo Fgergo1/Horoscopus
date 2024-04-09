756ba6e3582084ecf71f1472ca3a2ac5d00bb9cd
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-04-09 10:45:24 +0200

    date reserver function has created and passed to other components

--- frontend/horoscopus-frontend/src/pages/date-reserve/DateReserveForm.tsx
+++ frontend/horoscopus-frontend/src/pages/date-reserve/DateReserveForm.tsx
@@ -12,6 +12,25 @@ function DateReserveForm() {
         getFreeDates()
     }, []);
 
+    async function reserveDateById(id : number | undefined) {
+        const response = await fetch("/api/date/reserve", {
+            method : "POST",
+            headers : {
+                "content-type" : "application/json"
+            },
+            body : JSON.stringify(id)
+
+        })
+
+        if (response.status === 200) {
+            setFreeDates((freeDates) => {
+                return freeDates.filter((date) => date.id !== id)
+            })
+        } else {
+            setError("something goes wrong, try it later")
+        }
+    }
+
 
     async function getFreeDates() {
         const response = await fetch("/api/date/free", {
@@ -31,7 +50,7 @@ function DateReserveForm() {
 
 
     return (
-        <DateReservePage dates={freeDates} error={error}/>
+        <DateReservePage dates={freeDates} onReserve={reserveDateById} error={error}/>
     )
 }
 
