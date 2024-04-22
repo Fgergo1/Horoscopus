04ae5d8409adc990d599b2e0dfde097e3c263f85
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-04-22 13:21:17 +0200

    Transaction is enabled

--- backend/src/main/java/org/example/horoscopus/HoroscopusApplication.java
+++ backend/src/main/java/org/example/horoscopus/HoroscopusApplication.java
@@ -2,8 +2,10 @@ package org.example.horoscopus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class HoroscopusApplication {
 
 	public static void main(String[] args) {
