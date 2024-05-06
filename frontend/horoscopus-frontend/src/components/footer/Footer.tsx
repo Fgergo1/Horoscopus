202bdc0f2c978aed0de41a7ad0c52fba71a8ce4c
Farkas Gergo <128323984+Fgergo1@users.noreply.github.com>
2024-05-06 10:21:07 +0200

    footer component and style created

---
+++ frontend/horoscopus-frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+
+import "./Footer.css"
+function Footer () {
+
+    return (
+        <footer className="footer-container">
+            <p className="footer-text">Contact : horoscopus@gmail.com</p>
+            <a href="/terms">Terms</a>
+            <p className="footer-text">© Horoscopus.com, All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer
